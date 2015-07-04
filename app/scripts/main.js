0e76210780960412472925cff2198e9dc0629d88
John Ellison <info@johnwebsites.com>
2015-07-04 17:25:44 +0100

    add slick slider and begin styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,6 +8,14 @@
 
     $('span.zoom').zoom({url: 'images/downing-community-detailed-zoom.jpg'});
 
+    $('.l-project-slider').slick({
+      dots: true,
+      arrows: true,
+      infinite: true,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
+
   }(jQuery));
 
 }());
