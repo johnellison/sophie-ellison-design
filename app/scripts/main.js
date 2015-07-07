4a9eeb43fe281db3c10af3deac33f448a0971137
John Ellison <info@johnwebsites.com>
2015-07-07 17:28:33 +0100

    addd project photos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,12 @@
     $(document).foundation();
 
     $('#detailed').zoom({url: 'images/downing-community-detailed@3x.jpg'});
+    $('#concept').zoom({url: 'images/downing-community-concept@3x.jpg'});
     $('#context').zoom({url: 'images/downing-community-context@3x.jpg'});
+    $('#design').zoom({url: 'images/downing-community-design@3x.jpg'});
+    $('#keynote').zoom({url: 'images/downing-community-keynote@3x.jpg'});
+
+
 
     $('.l-project-slider').slick({
       dots: true,
