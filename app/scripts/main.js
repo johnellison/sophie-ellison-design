cfa120c2891a2e4001ac0e1c6e9eea3f784c865a
John Ellison <info@johnwebsites.com>
2015-07-07 22:26:33 +0100

    extract photos array to external data source

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,17 +6,17 @@
   (function($){
     $(document).foundation();
 
-    var photos = ['detailed', 'concept', 'context', 'design', 'keynote', 'perspective'];
-
     function zoomPhoto(name) {
       $('.zoom[data-zoom="' + name + '"]').zoom({
         url: 'images/downing-community-' + name + '@3x.jpg'
       });
     }
 
-    for (var i = 0; i < photos.length; i++) {
-      zoomPhoto(photos[i]);
-    }
+    $.getJSON( 'data/photos.json', function(data) {
+      for (var i = 0; i < data.photos.length; i++) {
+        zoomPhoto(data.photos[i]);
+      }
+    });   
 
     $('.l-project-slider').slick({
       dots: true,
