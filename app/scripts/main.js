784aa4e05b49a071e82ce301df46555fc216e7f1
John Ellison <info@johnwebsites.com>
2015-07-21 17:22:52 +0100

    add custom event tracking for google analytics and update tracking code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,47 @@
   (function($){
     $(document).foundation();
 
+    $("[data-event='contact-link']").on('click', function(){
+      ga('send', {
+        'hitType' : 'event',
+        'eventCategory' : 'Contact',
+        'eventAction' : 'sent',
+        'eventLabel' : 'sophie@sophie-ellison.com',
+        'eventValue' : 250
+      });
+    });
+
+    $("[data-event='download-cv']").on('click', function(){
+      ga('send', {
+        'hitType' : 'event',
+        'eventCategory' : 'Download',
+        'eventAction' : 'sent',
+        'eventLabel' : 'Download CV',
+        'eventValue' : 50
+      });
+    });
+
+    $("[data-event='download-downing-pdf']").on('click', function(){
+      ga('send', {
+        'hitType' : 'event',
+        'eventCategory' : 'Download',
+        'eventAction' : 'sent',
+        'eventLabel' : 'Download downing-community.pdf',
+        'eventValue' : 50
+      });
+    });
+
+    $("[data-event='view-inspiration-pdf']").on('click', function(){
+      ga('send', {
+        'hitType' : 'event',
+        'eventCategory' : 'Download',
+        'eventAction' : 'sent',
+        'eventLabel' : 'Inspiration (.pdf)',
+        'eventValue' : 50
+      });
+    });
+
+
   }(jQuery));
 
 }());
